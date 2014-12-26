b2f119feec705a6ebc59a1b67890a93c0e50bec9
Franco Nori <francong2@gmail.com>
2014-12-26 19:06:55 -0430

    arreglos varios en links

--- herramientas/herramientas/apps/main/forms.py
+++ herramientas/herramientas/apps/main/forms.py
@@ -4,3 +4,20 @@ from models import *
 from django.forms.extras.widgets import *
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.models import inlineformset_factory
+
+#Formulario de busqueda y filtrado
+class BusquedaForm(forms.Form):
+
+	choices_alquiler = (
+		('','Venta/Alquiler'),
+		('Alquiler', 'Alquiler'), 
+		('Compra','Venta'),
+		)
+
+	venta_alquiler = forms.ChoiceField(choices=choices_alquiler, required=True, widget=forms.Select(attrs={'class': "form-control"}))
+	categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), empty_label=' - Categoria -')
+	marca = forms.ModelChoiceField(queryset=Marca.objects.all(), empty_label=' - Marca -')
+	estado = forms.ModelChoiceField(queryset=Estado.objects.all(), empty_label=' - Estado -')
+	ciudad = forms.ModelChoiceField(queryset=Ciudad.objects.all(), empty_label=' - Ciudad -')
+	zona = forms.ModelChoiceField(queryset=Zona.objects.all(), empty_label=' - Zona -')
+
