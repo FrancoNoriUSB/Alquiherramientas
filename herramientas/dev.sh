e4988b966b9270c33d01599246ebc7a0e741f4c3
Franco Nori <francong2@gmail.com>
2015-01-22 15:23:48 -0430

    cambios en settings y server

--- herramientas/dev.sh
+++ herramientas/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash          
           rm -rf herramientas/settings/settings.py
-          cp herramientas/settings/settings_dev.py herramientas/settings/settings.py 
+          cp herramientas/settings/development.py herramientas/settings/settings.py 
--- herramientas/server.sh
+++ herramientas/server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash          
           rm -rf herramientas/settings/settings.py
-          cp herramientas/settings/settings_serv.py herramientas/settings/settings.py   
+          cp herramientas/settings/production.py herramientas/settings/settings.py   
