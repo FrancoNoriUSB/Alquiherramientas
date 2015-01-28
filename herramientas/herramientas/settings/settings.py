f25c40483c1e7fb5eabd24a9f0c4babe712e0855
Franco Nori <francong2@gmail.com>
2015-01-28 17:43:51 -0430

    Cambios varios en registro de usuario y detalles de la base

--- herramientas/herramientas/settings/settings.py
+++ herramientas/herramientas/settings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'herramientas.apps.administrador',
     'herramientas.apps.main',
     'widget_tweaks',
