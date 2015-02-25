11805c67a598d1b2d8e408a4ceb3b93f50fdf829
Franco Nori <francong2@gmail.com>
2015-02-25 16:03:05 -0430

    Arreglo de estilos y precio boton mcpgo

--- herramientas/herramientas/apps/main/views.py
+++ herramientas/herramientas/apps/main/views.py
@@ -472,8 +472,8 @@ def pagar(request, id_producto):
             email_venta(request, usuario.nombre, usuario.apellido, usuario.email, nombre)
             if precio > 100000.00:
                 return HttpResponseRedirect('/datos/')
-            else:
-                boton = mercadopago(request, nombre, float(precio))
+            
+            boton = mercadopago(request, nombre, float(precio))
         except:
             alquilerF = AlquilerForm(request.POST)
             if alquilerF.is_valid():
@@ -484,8 +484,8 @@ def pagar(request, id_producto):
                 email_alquiler(request, usuario.nombre, usuario.apellido, usuario.email, nombre, dias)
                 if total > 100000.00:
                     return HttpResponseRedirect('/datos/')
-                else:
-                    boton = mercadopago(request, nombre, float(precio))
+                
+                boton = mercadopago(request, nombre, float(precio))
 
     ctx = {
         'BusquedaForm':busquedaF,
