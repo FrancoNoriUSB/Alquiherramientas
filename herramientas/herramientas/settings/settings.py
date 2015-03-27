173209078743fc478351f98800a117ee1422e5dd
Franco Nori <francong2@gmail.com>
2015-03-26 22:27:03 -0430

    cambios admin

--- herramientas/herramientas/settings/settings.py
+++ herramientas/herramientas/settings/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'herramientas',                      # Or path to database file if using sqlite3.
         #The following settings are not used with sqlite3:
-        'USER': 'herramientas_user',
-        'PASSWORD': '@123456#',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
