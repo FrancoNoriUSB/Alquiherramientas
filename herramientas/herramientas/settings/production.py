56348ab40fce3a86f4b9070756654e6ac0d1dc7f
Franco Nori <francong2@gmail.com>
2015-04-01 22:42:44 -0430

    Dbug

--- herramientas/herramientas/settings/production.py
+++ herramientas/herramientas/settings/production.py
@@ -30,7 +30,7 @@ MAILCHIMP_WEBHOOK_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
